f7e096e5db09a53b86c4b1b0776212909f4b8f6a
Zalfa <mulkizalpa@gmail.com>
2026-05-16 20:59:50 +0700

    clean up next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
 };
 
 export default nextConfig;
